90216aa584def3421c562f755c7afac0e90bf4d6
toshi0607 <s.toshi0607@gmail.com>
2016-01-26 00:30:56 +0900

    ~p28

--- c1/scripts/app.js
+++ c1/scripts/app.js
@@ -1,10 +1,33 @@
 function AppModel(attrs) {
   this.val = "";
+  this.attrs = {
+    required: "",
+    maxlength: 8,
+    minlength: 4
+  };
   this.listeners = {
     valid: [],
     invalid: []
   };
 }
+
+
+AppModel.prototype.set = function(val) {
+  if (this.val === val) return;
+  this.validate();
+};
+
+AppModel.prototype.validate = function() {
+  var val;
+  this.errors = [];
+
+  for (var key in this.attrs) {
+    val = this.attrs[key];
+    if (!this[key](val)) this.errors.push(key);
+  }
+
+  this.trigger(!this.errors.length ? "valid" : "invalid");
+};
 
 AppModel.prototype.on = function(event, func) {
   this.listeners[event].push(func);
@@ -16,3 +39,39 @@ AppModel.prototype.trigger = function(event) {
   });
 };
 
+AppModel.prototype.required = function() {
+  return this.val !== "";
+};
+
+AppModel.prototype.maxlength = function(num) {
+  return num >= this.val.length;
+};
+
+AppModel.prototype.minlength = function(num) {
+  return num <= this.val.length;
+};
+
+function AppView(el) {
+  this.initialize(el);
+  this.handleEvents();
+}
+
+AppView.prototype.initialize = function(el) {
+  this.$el = $(el);
+
+  var obj = this.$el.data();
+
+  if (this.$el.prop("required")) {
+    obj["required"] = "";
+  }
+
+  this.model = new AppModel(obj);
+};
+
+AppView.prototype.handleEvents = function() {
+  var self = this;
+
+  this.$el.on("keyup", function(e) {
+    self.onKeyup(e);
+  });
+};
