b89b531a2572eee719a2ea9da05dc9b35891f177
toshi0607 <s.toshi0607@gmail.com>
2016-02-04 01:56:22 +0900

    fix typo

--- c3/index.js
+++ c3/index.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById( "canvas" );
-var ctx = canvas.getContext( "2d" ); 
+var ctx = canvas.getContext( "2d" );
 
 ctx.beginPath();
+ctx.fillStyle = "red";
 ctx.rect( 0, 0, 100, 200 );
 ctx.fill();
 ctx.closePath();
