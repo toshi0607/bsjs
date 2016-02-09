1ee5fcc950e9c7f1beeff2c842f65dac9c7f392a
toshi0607 <s.toshi0607@gmail.com>
2016-02-10 01:34:30 +0900

    ~p76

--- c3/index.js
+++ c3/index.js
@@ -7,11 +7,17 @@ window.requestAnimationFrame =
 
 var canvas = document.getElementById( "canvas" );
 var ctx = canvas.getContext( "2d" );
+var NUM = 20;
+var particles = [];
 
-var interval = Math.floor(1000/60);
+canvas.width = canvas.height = 500;
 
-var particle1 = new Particle(ctx, 0, 0);
-var particle2 = new Particle(ctx, 100, 5);
+for(var i = 0; i < NUM; i++) {
+  positionX =  Math.random()*120; // X座標を0-20の間でランダムに
+  positionY =  Math.random()*20; // Y座標を0-20の間でランダムに
+  particle = new Particle(ctx, positionX, positionY);
+  particles.push( particle );
+}
 
 function Particle(ctx, x, y) {
   this.ctx = ctx;
@@ -19,32 +25,39 @@ function Particle(ctx, x, y) {
   this.y = y || 0;
 }
 
-Particle.prototype.render = function() {
+Particle.prototype.render = function(){
   this.updatePosition();
   this.draw();
 };
 
-Particle.prototype.draw = function() {
+Particle.prototype.draw = function(){
+  // 4. 描画
   ctx = this.ctx;
   ctx.beginPath();
   ctx.fillStyle = "#99ff66";
-  ctx.rect(this.x, this.y, 10, 20);
+  ctx.rect( this.x, this.y, 4, 4 ); // 位置指定
   ctx.fill();
-  ctx.clothPath();
+  ctx.closePath();
 };
 
 Particle.prototype.updatePosition = function() {
+  // 3. 位置をずらす
   this.x += 5;
   this.y += 5;
 };
 
+// 1.図形を描画
+// 描画サイクルを開始する
 render();
 
 function render() {
-  ctx.clearRect(0, 0, 500, 500);
+  // 2.一度消去
+  ctx.clearRect(0,0,500,500); // 前回までの描画内容を消去
 
-  particle1.render();
-  particle2.render();
+  // 配列の各要素の関数renderを実行して図形を描画
+  particles.forEach(function(e){ e.render(); });
 
-  requestAnimationFrame(render);
+  // 5.一定間隔をおく
+  // requestanimationframeをつかって、ブラウザの更新のタイミングに実行する
+  requestAnimationFrame( render );
 }
