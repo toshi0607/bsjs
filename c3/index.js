380c758b77b827153e330a6a77d81cfc65323558
toshi0607 <s.toshi0607@gmail.com>
2016-02-10 01:20:43 +0900

    ~p75

--- c3/index.js
+++ c3/index.js
@@ -1,8 +1,3 @@
-var canvas = document.getElementById( "canvas" );
-var ctx = canvas.getContext( "2d" );
-
-var interval = Math.floor(1000/60);
-
 window.requestAnimationFrame =
   window.requestAnimationFrame ||
   window.mozRequestAnimationFrame ||
@@ -10,34 +5,46 @@ window.requestAnimationFrame =
   window.msRequestAnimationFrame ||
   function(cb) {setTimeout(cb, 17);};
 
-var x1 = 5;
-var y1 = 5;
-var x2 = 100;
-var y2 = 5;
+var canvas = document.getElementById( "canvas" );
+var ctx = canvas.getContext( "2d" );
+
+var interval = Math.floor(1000/60);
+
+var particle1 = new Particle(ctx, 0, 0);
+var particle2 = new Particle(ctx, 100, 5);
+
+function Particle(ctx, x, y) {
+  this.ctx = ctx;
+  this.x = x || 0;
+  this.y = y || 0;
+}
+
+Particle.prototype.render = function() {
+  this.updatePosition();
+  this.draw();
+};
+
+Particle.prototype.draw = function() {
+  ctx = this.ctx;
+  ctx.beginPath();
+  ctx.fillStyle = "#99ff66";
+  ctx.rect(this.x, this.y, 10, 20);
+  ctx.fill();
+  ctx.clothPath();
+};
+
+Particle.prototype.updatePosition = function() {
+  this.x += 5;
+  this.y += 5;
+};
 
 render();
 
 function render() {
   ctx.clearRect(0, 0, 500, 500);
 
-  updatePosition();
-  draw(x1, y1);
-  draw(x2, y2);
+  particle1.render();
+  particle2.render();
 
   requestAnimationFrame(render);
-}
-
-function updatePosition() {
-    x1 += 5;
-    y1 += 5;
-    x2 += 5;
-    y2 += 5;
-  }
-
-function draw(posx, posy) {
-  ctx.beginPath();
-  ctx.fillStyle = "#99ff66";
-  ctx.rect(posx, posy, 10, 20);
-  ctx.fill();
-  ctx.closePath();
 }
